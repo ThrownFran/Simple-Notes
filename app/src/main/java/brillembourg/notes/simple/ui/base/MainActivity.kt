ac2404cd3f800717b63055c172ee4ba6f4af57bc
franciscobrillembourg <franbrillembourg@gmail.com>
2022-08-09 19:07:42 -0400

    drawer layout

--- app/src/main/java/brillembourg/notes/simple/ui/base/MainActivity.kt
+++ app/src/main/java/brillembourg/notes/simple/ui/base/MainActivity.kt
@@ -7,9 +7,12 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import brillembourg.notes.simple.R
+import brillembourg.notes.simple.databinding.ActivityMainBinding
 import brillembourg.notes.simple.ui.extras.setBackgroundDrawable
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -17,9 +20,12 @@ class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var navController: NavController
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         setupToolbar()
     }
 
@@ -27,9 +33,31 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.container) as NavHostFragment
         navController = navHostFragment.navController
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setSupportActionBar(findViewById(R.id.toolbar))
-        setupActionBarWithNavController(navController, appBarConfiguration)
+//        appBarConfiguration = AppBarConfiguration.Builder(
+//            R.id.homeFragment,
+//            R.id.trashFragment,
+//        )
+//            .setDrawerLayout(binding.drawerLayout)
+//            .build()
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homeFragment, R.id.trashFragment
+            ), binding.drawerLayout
+        )
+
+//        appBarConfiguration = AppBarConfiguration(
+//            navGraph = navController.graph,
+//            drawerLayout = binding.drawerLayout
+//        )
+        setSupportActionBar(binding.toolbar)
+
+        setupActionBarWithNavController(
+            navController = navController,
+            configuration = appBarConfiguration
+        )
+
+        binding.navView.setupWithNavController(navController)
         supportActionBar?.setBackgroundDrawable(R.drawable.blue_creyon_2)
     }
 
